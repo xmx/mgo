2c9ac111ff6c8a07eb6da5c5da07b194e3f8fbaa
王兆运 <wangzhaoyun@eastmoney.com>
2021-05-26 19:19:30 +0800

    init

--- bucket.go
+++ bucket.go
@@ -3,27 +3,20 @@ package mgo
 import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/gridfs"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"time"
 )
-
-type BucketFile struct {
-	ID         interface{} `bson:"_id"`
-	Length     int64       `bson:"length"`
-	ChunkSize  int32       `bson:"chunkSize"`
-	UploadDate time.Time   `bson:"uploadDate"`
-	Name       string      `bson:"filename"`
-	Metadata   bson.Raw    `bson:"metadata"`
-}
 
 type BucketOperator struct {
 	*gridfs.Bucket
 }
 
 // File 根据文件ID获取文件的信息
-func (o *BucketOperator) File(id interface{}, opts ...*options.FindOneOptions) (BucketFile, error) {
+func (o *BucketOperator) File(id interface{}) (*gridfs.File, error) {
 	oid := parseID(id)
-	var bf BucketFile
-	err := o.Bucket.GetFilesCollection().FindOne(nil, oid, opts...).Decode(&bf)
-	return bf, err
+	cursor, err := o.Bucket.GetFilesCollection().Find(nil, bson.E{Key: "_id", Value: oid})
+	if err != nil {
+		return nil, err
+	}
+	f := &gridfs.File{}
+	err = cursor.Decode(f)
+	return f, err
 }
